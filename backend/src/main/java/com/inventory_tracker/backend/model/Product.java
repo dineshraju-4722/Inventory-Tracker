30bff58c4fe4a7523c5d2d695dc9dfacc71045c5
Dinesh-Raju <dineshsandireddy08@gmail.com>
2026-06-18 16:54:04 +0530

    Initialized basic three entities

---
+++ backend/src/main/java/com/inventory_tracker/backend/model/Product.java
@@ -0,0 +1,23 @@
+package com.inventory_tracker.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    private String productName;
+
+
+
+}
---
+++ backend/src/main/java/com/inventory_tracker/backend/model/Supplier.java
@@ -0,0 +1,26 @@
+package com.inventory_tracker.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "suppliers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Supplier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long supplierId;
+
+    private String companyName;
+
+    private String email;
+
+    private Integer pinCode;
+
+
+}
